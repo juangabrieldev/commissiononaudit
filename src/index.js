b0e52e2e054f9a4b095c7f560506eb2ad82b1057
AkoAngAdmin-PC <juangabrielpalarpalar@gmail.com>
2018-05-26 18:28:50 +0800

    added store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
 import Layout from './components/layout/layout';
+import { reducer } from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
 registerServiceWorker();
