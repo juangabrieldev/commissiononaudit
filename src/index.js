9e3939ae297f55686e0b276eaa2b564421294cb5
AkoAngAdmin-PC <juangabrielpalarpalar@gmail.com>
2018-05-26 18:38:13 +0800

    added provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import Layout from './components/layout/layout';
 import { reducer } from './store/reducer';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><Layout /></Provider>, document.getElementById('root'));
 registerServiceWorker();
