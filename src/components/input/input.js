1aa8d618f70d902f11f9004712c9ddfeba9a4679
AkoAngAdmin-PC <juangabrielpalarpalar@gmail.com>
2018-05-31 02:01:28 +0800

    modified internal

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -6,15 +6,16 @@ import styles from './input.scss';
 
 class Input extends Component {
   state = {
-    value: '',
+    value: this.props.value,
     borderColor: '#cdcdcd',
     didFocus: false,
-    onFocus: false
+    onFocus: false,
+    show: false
   };
 
   onChangeHandler = e => {
     this.setState({value: e.target.value});
-    this.props.onChangeHandler(e)
+    this.props.onChangeHandler(e);
   };
 
   onFocus = () => {
@@ -22,50 +23,71 @@ class Input extends Component {
   };
 
   onBlur = () => {
-    this.setState({borderColor: '#cdcdcd', didFocus: true, onFocus: false})
+    this.setState({borderColor: '#cdcdcd', didFocus: true, onFocus: false});
   };
 
   render() {
+    let border = '';
+
+    if(this.props.valid) {
+      border = 'solid 1px #30AF60'
+    } else if(this.props.error) {
+      border = 'solid 1px #ff6975'
+    } else {
+      border = 'solid 1px ' + this.state.borderColor
+    }
+
     return (
       <Aux>
         <div
-          style={{width: this.props.width, borderColor: this.state.borderColor}}
-          className={`${styles.div} ` +
-            (this.state.value.length >= 1 ? `${styles.hasValue} ` : ' ') +
-            (this.props.isValid ? styles.valid : '')}>
+          className={styles.input}
+          style={{
+            border, //checks if the input is valid
+            width: this.props.width
+          }}>
           <label
-            className={(this.props.error || (this.state.didFocus && this.state.value.length < this.props.requiredChar) ? styles.labelError : '') +
-                       (this.props.isValid ? styles.labelValid : '')
-            }
+            className={this.state.value.length > 0 ? styles.shown : ''}
+            style={{color: this.props.valid ? '#30AF60' : null}}
             htmlFor={this.props.name}>
-            {this.props.name}
+              {this.props.name}
           </label>
           <input
-            className={(this.props.error || (this.state.didFocus && this.state.value.length < this.props.requiredChar) ? styles.error : '') +
-                       (this.props.valid ? styles.valid : '')
-            }
-            onFocus={this.onFocus}
-            onBlur={this.onBlur}
-            onKeyUp={this.props.onKeyUp}
+            style={{
+              width: (this.props.type === 'password' && this.state.value.length > 0 ?
+                      this.props.width - 48 : ''
+              )
+            }}
             autoFocus={this.props.autofocus}
             onChange={e => this.onChangeHandler(e)}
-            name={this.props.name}
-            type={this.props.type}
-            placeholder={this.props.name}/>
+            onFocus={this.onFocus}
+            onBlur={this.onBlur}
+            type={this.state.show ? 'text' : this.props.type}
+            id={this.props.name}
+            placeholder={this.props.name}
+            value={this.state.value}/>
           {
-            this.props.error || (this.state.didFocus && this.state.value.length < this.props.requiredChar) ?
-              <div className={styles.message} style={{left: this.props.width + 8}}>
-                <p className={styles.error}>{this.props.errorMessage}</p>
+            this.props.type === 'password' && this.state.value.length > 0 ?
+              <div
+                className={styles.show}
+                onMouseDown={() => this.setState({show: !this.state.show})}
+                onMouseUp={() => this.setState({show: !this.state.show})}>
+                <p style={{
+                  transition: '.1s',
+                  color: this.state.show ? '#3268f0' : '',
+                  opacity: this.state.show ? 1 : ''
+                }}>SHOW</p>
               </div> :
               null
           }
-          {
-            this.props.isValid ?
-              <div className={styles.validLabel}>
-                <p>{this.props.validMessage}</p>
-              </div> :
-              null
-          }
+          {/*{*/}
+            {/*this.props.valid ?*/}
+              {/*<div className={`${styles.validationLabel} ${styles.valid}`}>*/}
+                {/*<p>{this.props.validationMessage}</p>*/}
+              {/*</div> :*/}
+              {/*<div className={`${styles.validationLabel} ${styles.error}`}>*/}
+                {/*<p>{this.props.validationMessage}</p>*/}
+              {/*</div>*/}
+          {/*}*/}
         </div>
       </Aux>
     );
